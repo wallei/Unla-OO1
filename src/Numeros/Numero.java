a042c330722c985c2228d044f79f028f63f5a56b
Walter Gazzanego <wgazzanego@gmail.com>
2022-08-16 21:11:58 -0300

    Primer commit

--- src/Numeros/Numero.java
+++ src/Numeros/Numero.java
@@ -1,5 +1,28 @@
 package Numeros;
 
 public class Numero {
+	private int numero;
+
+	public Numero(int numero) {
+		super();
+		this.numero = numero;
+	}
+
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	@Override
+	public String toString() {
+		return "Numero [numero=" + numero + "]";
+	}
+
+	
+	
+	
 
 }
--- src/Numeros/TestNumero.java
+++ src/Numeros/TestNumero.java
@@ -3,7 +3,8 @@ package Numeros;
 public class TestNumero {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println("impresion");
 
 	}
 
